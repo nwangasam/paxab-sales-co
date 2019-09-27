b78fd72e64b79896d56dec405d41965e56773681
nwangasam <46712385+nwangasam@users.noreply.github.com>
2019-09-27 23:07:06 +0100

    added the css

---
+++ src/js/test.js
@@ -0,0 +1,8 @@
+export default {
+    name: "Omega",
+    age: 29,
+    dept: "Mechanical Engineering",
+    state: "Imo",
+    hobbies: ["Coding", "Typing", "Problem solving", "thinking"],
+    isMarried: false
+}
